0b43beca239f63f15012e3b30f5c162c6e305172
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2024-07-22 09:22:37 +0530

    chnge server

--- new_app/client/src/pages/Login.jsx
+++ new_app/client/src/pages/Login.jsx
@@ -22,7 +22,7 @@ function Login() {
   const { email, password } = formData;
 
   useEffect(() => {
-    if (isError) toast.error(message.response.data);
+    if (isError) toast.error(message);
     if (isSuccess) {
       navigate("/");
       toast.success("Login Successfull");
