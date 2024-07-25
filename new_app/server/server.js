6a81966cad6829eb2bab3d5ff54ccbaa9a1563f8
vishnaka-dasanayaka <vishnakadasanayaka@gmail.com>
2024-07-25 14:10:45 +0530

    test 4

--- new_app/server/server.js
+++ new_app/server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 //static path
 const staticFilePath = path.join(__dirname, 'public');
