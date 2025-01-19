d356cfe51aca87e7176c3f9a545de8dac4aefa77
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2025-01-19 12:15:52 +0530

    Order Update Function Done

--- new_app/server/controllers/orderController.js
+++ new_app/server/controllers/orderController.js
@@ -1,241 +1,318 @@
-const asyncHandler = require('express-async-handler')
-const Order = require('../models/orderModel')
-const User = require('../models/userModel')
-const Patient = require('../models/patientModel')
-const Prescription = require('../models/prescriptionModel')
+const asyncHandler = require("express-async-handler");
+const Order = require("../models/orderModel");
+const User = require("../models/userModel");
+const Patient = require("../models/patientModel");
+const Prescription = require("../models/prescriptionModel");
+const path = require("path");
+const fs = require("fs");
 
 const getActiveOrders = asyncHandler(async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id)
+  try {
+    const user = await User.findById(req.user.id);
 
-        if (!user) {
-            res.status(400)
-            throw new Error('User not found')
-        }
-
-        const orders = await Order.find({ isDeleted: false });
-
-        if (orders) {
-            res.status(200).json(orders)
-        } else {
-            res.status(400)
-            throw new Error('no active orders')
-        }
-    } catch (error) {
-        res.status(500).json(error)
+    if (!user) {
+      res.status(400);
+      throw new Error("User not found");
     }
-})
+
+    const orders = await Order.find({ isDeleted: false });
+
+    if (orders) {
+      res.status(200).json(orders);
+    } else {
+      res.status(400);
+      throw new Error("no active orders");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 const findOrders = asyncHandler(async (req, res) => {
-    const { orderNumber, billNumber } = req.body
+  const { orderNumber, billNumber } = req.body;
 
+  let user;
 
-    let user;
+  try {
+    user = await User.findById(req.user.id);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
 
-    try {
-        user = await User.findById(req.user.id)
-    } catch (error) {
-        res.status(500).json('Server Error')
-    }
+  if (!user) {
+    res.status(400).json("User not found");
+  }
 
-    if (!user) {
-        res.status(400).json('User not found')
-    }
+  let filter = {};
 
-    let filter = {}
+  // if (orderNumber !== null && orderNumber !== '') filter.orderNumber = orderNumber
+  //  if (billNumber !== null && billNumber !== '') filter.billNumber = billNumber
 
-    // if (orderNumber !== null && orderNumber !== '') filter.orderNumber = orderNumber
-    //  if (billNumber !== null && billNumber !== '') filter.billNumber = billNumber
+  if (orderNumber !== null && orderNumber !== "") {
+    const orderNumberRegex = new RegExp(orderNumber.trim().toLowerCase(), "i"); // 'i' for case-insensitive
+    filter.orderNumber = { $regex: orderNumberRegex };
+  }
 
-    if (orderNumber !== null && orderNumber !== '') {
-        const orderNumberRegex = new RegExp(orderNumber.trim().toLowerCase(), 'i'); // 'i' for case-insensitive
-        filter.orderNumber = { $regex: orderNumberRegex };
-    }
+  if (billNumber !== null && billNumber !== "") {
+    const billNumberRegex = new RegExp(billNumber.trim().toLowerCase(), "i"); // 'i' for case-insensitive
+    filter.billNumber = { $regex: billNumberRegex };
+  }
 
-    if (billNumber !== null && billNumber !== '') {
-        const billNumberRegex = new RegExp(billNumber.trim().toLowerCase(), 'i'); // 'i' for case-insensitive
-        filter.billNumber = { $regex: billNumberRegex };
-    }
+  const filteredOrders = await Order.find(filter).lean();
 
-    const filteredOrders = await Order.find(filter).lean();
+  const orders = filteredOrders.filter((order) => order.isDeleted !== true);
 
-    const orders = filteredOrders.filter(order => order.isDeleted !== true);
+  const patientIDs = orders.map((order) => order.patient);
 
+  const patients = await Patient.find({ _id: { $in: patientIDs } }).lean();
 
-    const patientIDs = orders.map(order => order.patient);
+  const patientMap = patients.reduce((acc, patient) => {
+    acc[patient._id] = patient;
+    return acc;
+  }, {});
 
-
-    const patients = await Patient.find({ _id: { $in: patientIDs } }).lean();
-
-
-    const patientMap = patients.reduce((acc, patient) => {
-        acc[patient._id] = patient;
-        return acc;
-    }, {});
-
-    const ordersWithPatientInfo = orders.map(order => {
-        const patient = patientMap[order.patient];
-        return {
-            ...order,
-            name: patient ? patient.name : null,
-            dob: patient ? patient.dob : null,
-            contactNumber: patient ? patient.contactNumber : null,
-        };
-    });
-    res.status(200).json(ordersWithPatientInfo)
-})
-
+  const ordersWithPatientInfo = orders.map((order) => {
+    const patient = patientMap[order.patient];
+    return {
+      ...order,
+      name: patient ? patient.name : null,
+      dob: patient ? patient.dob : null,
+      contactNumber: patient ? patient.contactNumber : null,
+    };
+  });
+  res.status(200).json(ordersWithPatientInfo);
+});
 
 const getOrder = asyncHandler(async (req, res) => {
+  let user;
 
-    let user;
+  try {
+    user = await User.findById(req.user.id);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
 
-    try {
-        user = await User.findById(req.user.id)
-    } catch (error) {
-        res.status(500).json('Server Error')
-    }
+  if (!user) {
+    res.status(400).json("User not found");
+  }
 
-    if (!user) {
-        res.status(400).json('User not found')
-    }
+  try {
+    const order = await Order.findById(req.params.id);
 
-    try {
-        const order = await Order.findById(req.params.id);
+    order.frameImg = order.frameImg
+      ? `${process.env.API_END_POINT}/Frames/${order.frameImg}`
+      : "";
 
-        order.frameImg = order.frameImg ? `${process.env.API_END_POINT}/Frames/${order.frameImg}` : ""
-
-        res.status(200).json(order)
-    } catch (error) {
-        res.status(500).json("sever Error")
-    }
-
-
-})
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json("sever Error");
+  }
+});
 
 const addOrder = asyncHandler(async (req, res) => {
-    const orderData = JSON.parse(req.body.orderData);
-    const presData = JSON.parse(req.body.presData);
-    const frameData = JSON.parse(req.body.frameData);
+  const orderData = JSON.parse(req.body.orderData);
+  const presData = JSON.parse(req.body.presData);
+  const frameData = JSON.parse(req.body.frameData);
 
+  if (
+    !orderData.date ||
+    !orderData.branch ||
+    !orderData.orderNumber ||
+    !orderData.status
+  ) {
+    res.status(400);
+    throw new Error("Please add order details");
+  }
+  const checkExist = await Order.findOne({
+    orderNumber: orderData.orderNumber,
+  });
 
+  if (checkExist) {
+    res.status(400);
+    throw new Error("Order number is alredy in the system");
+  }
 
-    if (!orderData.date || !orderData.branch || !orderData.orderNumber || !orderData.status) {
-        res.status(400)
-        throw new Error('Please add order details')
-    }
-    const checkExist = await Order.findOne({ orderNumber: orderData.orderNumber });
+  try {
+    const order = await Order.create({
+      patient: req.params.id,
+      date: orderData.date,
+      branch: orderData.branch,
+      orderNumber: orderData.orderNumber,
+      billNumber: orderData.billNumber,
+      lenses: orderData.lenses,
+      price: orderData.price,
+      advance: orderData.advance,
+      balance: orderData.balance,
+      status: orderData.status,
+      sentDate: orderData.sentDate,
+      receivedDate: orderData.receivedDate,
+      deliveredDate: orderData.deliveredDate,
+      specialNote: orderData.specialNote,
+      // frameImg: frameData ? req.files.frame_img[0].filename : "",
+      frameImg:
+        req.files && req.files.frame_img ? req.files.frame_img[0].filename : "",
+      frameDesc: frameData ? frameData.frameDescription : "",
+    });
 
-    if (checkExist) {
-        res.status(400)
-        throw new Error('Order number is alredy in the system')
+    const prescription = await Prescription.create({
+      patient: req.params.id,
+      order: order._id,
+      VAR: presData.VAR,
+      VAL: presData.VAL,
+      VARPH: presData.VARPH,
+      VALPH: presData.VALPH,
+      retiR: presData.retiR,
+      retiL: presData.retiL,
+      hbrxDate: presData.hbrxDate,
+      hbrxRSPH: presData.hbrxRSPH,
+      hbrxRCYL: presData.hbrxRCYL,
+      hbrxRAXIS: presData.hbrxRAXIS,
+      hbrxLSPH: presData.hbrxLSPH,
+      hbrxLCYL: presData.hbrxLCYL,
+      hbrxLAXIS: presData.hbrxLAXIS,
+      hbrxRSummary: presData.hbrxRSummary,
+      hbrxLSummary: presData.hbrxLSummary,
+      RSPH: presData.RSPH,
+      RCYL: presData.RCYL,
+      RAXIS: presData.RAXIS,
+      LSPH: presData.LSPH,
+      LCYL: presData.LCYL,
+      LAXIS: presData.LAXIS,
+      rSummary: presData.rSummary,
+      lSummary: presData.lSummary,
+      presNote: presData.presNote,
+      rvDate: presData.rvDate,
+      signedBy: presData.signedBy,
+      presImg:
+        req.files && req.files.pres_img ? req.files.pres_img[0].filename : "",
+      // presImg: req.files.pres_img[0].filename
+      // frameImg: frameData ? req.files.frame_img[0].filename : "",
+      // presImg: presData ? req.files.pres_img[0].filename : "",
+    });
 
-    }
+    const updatedPatient = await Patient.findByIdAndUpdate(
+      req.params.id,
+      { $push: { orders: order._id } },
+      { new: true }
+    );
 
-    try {
+    const final = await Patient.findByIdAndUpdate(
+      req.params.id,
+      { $push: { history: `New order added by ${req.user.firstName}` } },
+      { new: true }
+    );
 
-        const order = await Order.create({
-            patient: req.params.id,
-            date: orderData.date,
-            branch: orderData.branch,
-            orderNumber: orderData.orderNumber,
-            billNumber: orderData.billNumber,
-            lenses: orderData.lenses,
-            price: orderData.price,
-            advance: orderData.advance,
-            balance: orderData.balance,
-            status: orderData.status,
-            sentDate: orderData.sentDate,
-            receivedDate: orderData.receivedDate,
-            deliveredDate: orderData.deliveredDate,
-            specialNote: orderData.specialNote,
-            // frameImg: frameData ? req.files.frame_img[0].filename : "",
-            frameImg: req.files && req.files.frame_img ? req.files.frame_img[0].filename : "",
-            frameDesc: frameData ? frameData.frameDescription : ""
-        })
+    const updatedOrder = await Order.findByIdAndUpdate(
+      order._id,
+      { $push: { prescriptions: prescription._id } },
+      { new: true }
+    );
 
-        const prescription = await Prescription.create({
-            patient: req.params.id,
-            order: order._id,
-            VAR: presData.VAR,
-            VAL: presData.VAL,
-            VARPH: presData.VARPH,
-            VALPH: presData.VALPH,
-            retiR: presData.retiR,
-            retiL: presData.retiL,
-            hbrxDate: presData.hbrxDate,
-            hbrxRSPH: presData.hbrxRSPH,
-            hbrxRCYL: presData.hbrxRCYL,
-            hbrxRAXIS: presData.hbrxRAXIS,
-            hbrxLSPH: presData.hbrxLSPH,
-            hbrxLCYL: presData.hbrxLCYL,
-            hbrxLAXIS: presData.hbrxLAXIS,
-            hbrxRSummary: presData.hbrxRSummary,
-            hbrxLSummary: presData.hbrxLSummary,
-            RSPH: presData.RSPH,
-            RCYL: presData.RCYL,
-            RAXIS: presData.RAXIS,
-            LSPH: presData.LSPH,
-            LCYL: presData.LCYL,
-            LAXIS: presData.LAXIS,
-            rSummary: presData.rSummary,
-            lSummary: presData.lSummary,
-            presNote: presData.presNote,
-            rvDate: presData.rvDate,
-            signedBy: presData.signedBy,
-            presImg: req.files && req.files.pres_img ? req.files.pres_img[0].filename : ""
-            // presImg: req.files.pres_img[0].filename
-            // frameImg: frameData ? req.files.frame_img[0].filename : "",
-            // presImg: presData ? req.files.pres_img[0].filename : "",
-
-        })
-
-        const updatedPatient = await Patient.findByIdAndUpdate(
-            req.params.id,
-            { $push: { orders: order._id } },
-            { new: true }
-        );
-
-        const final = await Patient.findByIdAndUpdate(req.params.id, { $push: { history: `New order added by ${req.user.firstName}` } }, { new: true })
-
-        const updatedOrder = await Order.findByIdAndUpdate(
-            order._id,
-            { $push: { prescriptions: prescription._id } },
-            { new: true }
-        );
-
-
-        res.status(200).json({
-            patient: final,
-            order: updatedOrder,
-            prescription: prescription
-        })
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+    res.status(200).json({
+      patient: final,
+      order: updatedOrder,
+      prescription: prescription,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 const deleteOrder = asyncHandler(async (req, res) => {
-    let user;
+  let user;
 
-    try {
-        user = await User.findById(req.user.id)
-    } catch (error) {
-        res.status(500).json('Server Error')
+  try {
+    user = await User.findById(req.user.id);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+
+  if (!user) {
+    res.status(400).json("User not found");
+  }
+
+  try {
+    const order = await Order.findByIdAndUpdate(req.params.id, {
+      orderNumber: null,
+      isDeleted: true,
+    });
+
+    console.log(order);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(500).json("sever Error");
+  }
+});
+
+const updateOrder = asyncHandler(async (req, res) => {
+  let user;
+
+  try {
+    user = await User.findById(req.user.id);
+  } catch (error) {
+    res.status(500).json("Server Error");
+  }
+
+  if (!user) {
+    res.status(400).json("User not found");
+  }
+
+  try {
+    const order = await Order.findById(req.params.id);
+    const oldPic = order.frameImg;
+
+    const orderDetails = JSON.parse(req.body.orderData);
+
+    if (orderDetails.status === "sent") {
+      orderDetails.receivedDate = "";
+      orderDetails.deliveredDate = "";
+    } else if (orderDetails.status === "received") {
+      orderDetails.deliveredDate = "";
     }
 
-    if (!user) {
-        res.status(400).json('User not found')
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        lenses: orderDetails.lenses,
+        price: orderDetails.price,
+        advance: orderDetails.advance,
+        balance: orderDetails.price - orderDetails.advance,
+        specialNote: orderDetails.specialNote,
+        frameDesc: orderDetails.frameDesc,
+        status: orderDetails.status,
+        sentDate: orderDetails.sentDate,
+        deliveredDate: orderDetails.deliveredDate,
+        receivedDate: orderDetails.receivedDate,
+        frameImg:
+          req.files && req.files.frame_img
+            ? req.files.frame_img[0].filename
+            : "",
+      },
+      { new: true }
+    );
+
+    if (oldPic !== "") {
+      const oldPicPath = path.join(__dirname, "..", "public", "Frames", oldPic);
+      fs.unlink(oldPicPath, (err) => {
+        if (err) {
+          console.error("Error deleting old profile picture:", err);
+        }
+      });
     }
 
-    try {
-        const order = await Order.findByIdAndUpdate(req.params.id, { orderNumber: null, isDeleted: true });
+    res.status(201).json(updatedOrder);
+  } catch (error) {
+    console.log(error);
 
-        console.log(order)
-        res.status(200).json(order)
-    } catch (error) {
-        res.status(500).json("sever Error")
-    }
-})
+    res.status(500).json("sever Error");
+  }
+});
 
-module.exports = { getActiveOrders, findOrders, getOrder, addOrder, deleteOrder }
+module.exports = {
+  getActiveOrders,
+  findOrders,
+  getOrder,
+  addOrder,
+  deleteOrder,
+  updateOrder,
+};
