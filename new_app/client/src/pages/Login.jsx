c605a999eb80711b5911782f6838185196accb47
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2025-05-16 23:38:03 +0530

    remove 'under dev'

--- new_app/client/src/pages/Login.jsx
+++ new_app/client/src/pages/Login.jsx
@@ -49,54 +49,49 @@ function Login() {
 
   if (isLoading) return <Loading />;
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <h1>Ooops!</h1>
-      <h2>Sorry For the trouble...</h2>
-      <h3>Eye planet is under development </h3>
+    <div className="login_body">
+      <div class="   w-screen h-screen flex justify-center items-center backdrop-blur-sm   backdrop-brightness-50">
+        <div className="h-fit sm:w-[550px] w-full flex flex-col justify-center items-center  bg-white opacity-85 rounded-lg p-5">
+          <h1 className="text-3xl font-extrabold capitalize">login</h1>
+          <div className="flex items-center justify-between w-full p-3 my-3 mt-5 border-2 rounded-md">
+            <input
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Username"
+              className="w-full p-1 mr-2 outline-none"
+              type="text"
+            />
+            <PersonIcon />
+          </div>
+
+          <div className="flex items-center justify-between w-full p-3 my-3 border-2 rounded-md">
+            <input
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Password"
+              className="w-full p-1 mr-2 outline-none"
+              type="password"
+            />
+            <PasswordIcon />
+          </div>
+
+          <button onClick={onSubmit} className="w-full bg-red-400">
+            login
+          </button>
+
+          <h3 className="mt-2 text-sm font-semibold">
+            Forget password ?
+            <span className="ml-1 underline cursor-pointer hover:text-blue-500">
+              change password
+            </span>
+          </h3>
+        </div>
+      </div>
     </div>
-    // <div className="login_body">
-    //   <div class="   w-screen h-screen flex justify-center items-center backdrop-blur-sm   backdrop-brightness-50">
-    //     <div className="h-fit sm:w-[550px] w-full flex flex-col justify-center items-center  bg-white opacity-85 rounded-lg p-5">
-    //       <h1 className="text-3xl font-extrabold capitalize">login</h1>
-    //       <div className="flex items-center justify-between w-full p-3 my-3 mt-5 border-2 rounded-md">
-    //         <input
-    //           id="email"
-    //           name="email"
-    //           value={email}
-    //           onChange={onChange}
-    //           placeholder="Username"
-    //           className="w-full p-1 mr-2 outline-none"
-    //           type="text"
-    //         />
-    //         <PersonIcon />
-    //       </div>
-
-    //       <div className="flex items-center justify-between w-full p-3 my-3 border-2 rounded-md">
-    //         <input
-    //           id="password"
-    //           name="password"
-    //           value={password}
-    //           onChange={onChange}
-    //           placeholder="Password"
-    //           className="w-full p-1 mr-2 outline-none"
-    //           type="password"
-    //         />
-    //         <PasswordIcon />
-    //       </div>
-
-    //       <button onClick={onSubmit} className="w-full bg-red-400">
-    //         login
-    //       </button>
-
-    //       <h3 className="mt-2 text-sm font-semibold">
-    //         Forget password ?
-    //         <span className="ml-1 underline cursor-pointer hover:text-blue-500">
-    //           change password
-    //         </span>
-    //       </h3>
-    //     </div>
-    //   </div>
-    // </div>
   );
 }
 
