b752fc1a4e4eaae53f3369bde864266abdf6413a
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2026-01-08 21:18:17 +0530

    urls

--- new_app/client/src/environments/environment.prod.ts
+++ new_app/client/src/environments/environment.prod.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
-  // baseUrl: "http://45.13.132.116:1337/",
-  baseUrl: "https://sethneth.lk/",
-  // baseUrl: "http://localhost:1337/",
+  // baseUrl: "https://eyeplanet.lk/",
+  baseUrl: "http://localhost:1338/",
 };
 //
